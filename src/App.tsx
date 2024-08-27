fcb92001e4639d4f4cc023ba6be3de6466c09d69
tomtomdu73 <thomas.cosialls@gmail.com>
2024-08-27 13:55:14 -0500

    fix: typescript error with WebAppInitData

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [initData, setInitData] = useState<Telegram.WebAppInitData>({});
+  const [initData, setInitData] = useState<Telegram.WebAppInitData>();
 
   useEffect(() => {
     console.log("useTelegram");
