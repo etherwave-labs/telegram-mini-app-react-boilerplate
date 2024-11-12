c010b9025cd52ff8b30f808792d8094060d8c368
tomtomdu73 <thomas.cosialls@gmail.com>
2024-11-12 15:14:28 -0600

    fix: typescript error

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import * as Telegram from "@twa-dev/types";
-import WebApp from "@twa-dev/sdk";
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+// import * as Telegram from "@twa-dev/types"
+import WebApp from "@twa-dev/sdk"
 
-import reactLogo from "@/assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import reactLogo from "@/assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [initData, setInitData] = useState();
+  const [count, setCount] = useState(0)
+  const [initData, setInitData] = useState<string>()
 
   useEffect(() => {
-    const data = WebApp.initData;
+    const data = WebApp.initData
     if (Object.keys(data).length === 0 && data.constructor === Object) {
-      console.error("❌ Open this app in Telegram");
-      return;
+      console.error("❌ Open this app in Telegram")
+      return
     }
-    console.log("✅Telegram WebApp Available", data);
-    setInitData(data);
-    WebApp.expand();
-  }, []);
+    console.log("✅Telegram WebApp Available", data)
+    setInitData(data)
+    WebApp.expand()
+  }, [])
 
   return (
     <div className="flex flex-col gap-10">
@@ -45,18 +45,14 @@ function App() {
       </div>
 
       <div className="flex flex-col justify-center">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
